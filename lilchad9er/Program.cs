8b1f1658d4a193c86a545bd25e17dc74f2f40035
Hamed Radhouani <radhouani.h02@htlwienwest.at>
2022-06-07 11:53:01 +0200

    eigentlich fertig

--- lilchad9er/Program.cs
+++ lilchad9er/Program.cs
@@ -51,29 +51,9 @@ namespace pgdemo
                         }
                     }
 
-                    if (recievedMessage[0]== "login")
+                    if (recievedMessage[0] == "login")
                     {
-                        
 
-                        if(checklogin(recievedMessage[1], recievedMessage[2]))
-                        {
-                            string replay = login(recievedMessage[1]);
-
-
-                            SendToApp(replay, ns);
-
-                        }
-                        else
-                        {
-                            SendToApp("false;0", ns);
-                        }
-
-                        
-                    }
-
-                    if (recievedMessage[0] == "saveuser")
-                    {
-                        //Hamed dings neue methode erstellen. dass die daten von recievemessage[diese kack numemrn] in datenbank speichert
 
                         if (checklogin(recievedMessage[1], recievedMessage[2]))
                         {
@@ -91,6 +71,88 @@ namespace pgdemo
 
                     }
 
+                    if (recievedMessage[0] == "saveuser")
+                    {
+                        //Hamed dings neue methode erstellen. dass die daten von recievemessage[diese kack numemrn] in datenbank speichert
+
+
+                        int userid = Convert.ToInt32((string)recievedMessage[1]);
+                        int gewicht = Convert.ToInt32((string)recievedMessage[3]);
+                        int groesse = Convert.ToInt32((string)recievedMessage[4]);
+                        int alter = Convert.ToInt32((string)recievedMessage[5]);
+
+
+                        if(saveusermeth(userid, recievedMessage[2], gewicht, groesse, alter))
+                        {
+
+                            SendToApp($"true",ns);
+                        }
+                        else
+                        {
+                            SendToApp("false;0", ns);
+                        }                     
+                    }
+
+                    if (recievedMessage[0] == "setpetname")
+                    {
+                        //Hamed dings neue methode erstellen. dass die daten von recievemessage[diese kack numemrn] in datenbank speichert
+                        int userid = Convert.ToInt32((string)recievedMessage[1]);
+
+                        if(savepetname(userid, recievedMessage[2]))
+                        {
+                            SendToApp($"true", ns);
+
+                        }
+                        
+                        else
+                        {
+                            SendToApp("false;0", ns);
+                        }
+                    }
+
+                    if (recievedMessage[0] == "lvl")
+                    {
+    
+                        int userid = Convert.ToInt32((string)recievedMessage[1]);
+                        int levelup = Convert.ToInt32((string)recievedMessage[2]);
+                        var currentpetlvl  = getcrurentpetlvl(userid);
+
+                        //ändere savepetname lol
+
+
+                        if(savepetlvl(userid, levelup, currentpetlvl))
+                        {
+                            SendToApp($"true", ns);
+                        }                      
+
+                        else
+                        {
+                            SendToApp("false;0", ns);
+                        }
+                    }
+
+
+                    if (recievedMessage[0] == "upxp")
+                    {
+
+                        int userid = Convert.ToInt32((string)recievedMessage[1]);
+                        int xpup = Convert.ToInt32((string)recievedMessage[2]);
+                        var currentpetxp = getcurrentxp(userid);
+
+                        //ändere savepetname lol
+
+
+                        if (savepetxp(userid, xpup, currentpetxp))
+                        {
+                            SendToApp($"true", ns);
+                        }
+
+                        else
+                        {
+                            SendToApp("false;0", ns);
+                        }
+                    }
+
 
 
                 }, client);
@@ -99,7 +161,7 @@ namespace pgdemo
 
         }
 
-        private static bool SendToApp(string messageToSend,NetworkStream ns ) 
+        private static bool SendToApp(string messageToSend, NetworkStream ns)
         {
             try
             {
@@ -116,7 +178,7 @@ namespace pgdemo
                 Console.WriteLine(e);
                 throw;
             }
-            
+
         }
 
         private async static Task<string[]> RecieveFromAppAsync(NetworkStream ns) //gibt schon gesplittetes array zurück
@@ -167,7 +229,61 @@ namespace pgdemo
         }
 
 
-            private static string  login (string name)
+        private static int getcrurentpetlvl(int userid)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+
+                var cmd = new NpgsqlCommand("SELECT pet_level FROM users WHERE user_id = @p2  LIMIT 1;", con)
+                {
+                    Parameters =
+                    {
+                        new("p2", userid)
+                    }
+                };
+
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                Console.WriteLine(cmd.Parameters[0].Value);
+
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+                dr.Read();
+
+                var result = dr.GetInt32(0);
+
+                return result;
+            }
+        }
+
+        private static int getcurrentxp(int userid)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+
+                var cmd = new NpgsqlCommand("SELECT pet_xp FROM users WHERE user_id = @p2  LIMIT 1;", con)
+                {
+                    Parameters =
+                    {
+                        new("p2", userid)
+                    }
+                };
+
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                Console.WriteLine(cmd.Parameters[0].Value);
+
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+                dr.Read();
+
+                var result = dr.GetInt32(0);
+
+                return result;
+            }
+        }
+
+        private static string login(string name)
         {
             string finaloutput = "f";
             using (NpgsqlConnection con = GetConnection())
@@ -189,12 +305,12 @@ namespace pgdemo
                 {
                     string output = string.Format("{0} {1} {2} {3} {4} {5} {6}", reader.GetValue(5), reader.GetValue(6), reader.GetValue(7), reader.GetValue(1), reader.GetValue(4), reader.GetValue(3), reader.GetValue(8));
 
-                    string newoutput = output.Replace(' ',';');
+                    string newoutput = output.Replace(' ', ';');
 
-                    finaloutput =  "alldata;" + newoutput;
-                    
+                    finaloutput = "alldata;" + newoutput;
+
                 }
-                
+
                 return finaloutput;
 
             }
@@ -222,6 +338,193 @@ namespace pgdemo
                 if (n == 1)
                 {
                     Console.WriteLine("Insert ist erfolgreich :)");
+                }
+            }
+        }
+
+
+
+
+        private static bool savepetlvl(int userid, int lvlup,int currentlvl)
+        {
+
+            int geupdatetlvl = currentlvl + lvlup;
+            
+            using (NpgsqlConnection con = GetConnection())
+            {
+
+                var cmd4 = new NpgsqlCommand("UPDATE users SET pet_level = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", geupdatetlvl),
+
+                    }
+                };
+                con.Open();
+                int n4 = cmd4.ExecuteNonQuery();
+                if (n4 == 1)
+                {
+
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private static bool savepetxp(int userid, int xpup, int currentxp)
+        {
+
+            int geupdatetxp = currentxp + xpup;
+
+            using (NpgsqlConnection con = GetConnection())
+            {
+
+                var cmd4 = new NpgsqlCommand("UPDATE users SET pet_xp = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", geupdatetxp),
+
+                    }
+                };
+                con.Open();
+                int n4 = cmd4.ExecuteNonQuery();
+                if (n4 == 1)
+                {
+
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        private static bool savepetname(int userid, string petname)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+
+                var cmd4 = new NpgsqlCommand("UPDATE users SET pet_name = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", petname),
+
+                    }
+                };
+                con.Open();
+                int n4 = cmd4.ExecuteNonQuery();
+                if (n4 == 1)
+                {
+               
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+
+        private static bool saveusermeth(int userid, string username, int weight, int size, int age)
+        {
+            int verifirer = 0;
+            using (NpgsqlConnection con = GetConnection())
+            {
+
+                var cmd4 = new NpgsqlCommand("UPDATE users SET name = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", username),
+
+                    }
+                };
+                con.Open();
+                int n4 = cmd4.ExecuteNonQuery();
+                if (n4 == 1)
+                {
+                    verifirer += 1;
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                }
+
+                var cmd = new NpgsqlCommand("UPDATE users SET gewicht = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", weight),
+
+                    }
+                };
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 1)
+                {
+                    verifirer += 1;
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                }
+
+
+
+
+                var cmd1 = new NpgsqlCommand("UPDATE users SET groesse = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", size),
+
+                    }
+                };
+                con.Open();
+                int n1 = cmd1.ExecuteNonQuery();
+                if (n1 == 1)
+                {
+                    verifirer += 1;
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                }
+
+                var cmd3 = new NpgsqlCommand("UPDATE users SET age = @p2 WHERE user_id = @p1", con)
+                {
+                    Parameters =
+                    {
+                        new("p1", userid),
+                        new("p2", age),
+
+                    }
+                };
+                con.Open();
+                int n3 = cmd1.ExecuteNonQuery();
+                if (n3 == 1)
+                {
+                    verifirer += 1;
+                    Console.WriteLine("Insert ist erfolgreich :)");
+                }
+
+
+                if(verifirer == 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
             }
         }
